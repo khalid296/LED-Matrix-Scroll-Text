981d1d69fc410ef44c6f81f32dbbdf77b14d8a7d
“Khalid <khalid.tarek.95@gmail.com>
2018-11-28 13:45:49 +0200

    first commit

---
+++ micro_config.h
@@ -0,0 +1,25 @@
+ /******************************************************************************
+ *
+ * Module: Micro - Configuration
+ *
+ * File Name: Micro_Config.h
+ *
+ * Description: File include all Microcontroller libraries
+ *
+ * Author: Mohamed Tarek
+ *
+ *******************************************************************************/
+
+#ifndef MICRO_CONFIG_H_
+#define MICRO_CONFIG_H_
+
+#ifndef F_CPU
+#define F_CPU 1000000UL //1MHz Clock frequency
+#endif  
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/pgmspace.h>
+
+
+#endif /* MICRO_CONFIG_H_ */
